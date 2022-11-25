3ba07ecd92a643afe313ae2c6b92dbbd9b1098bc
Jonathan <jonathanfelipe.muller@gmail.com>
2022-11-25 20:11:31 -0300

    removing console.log

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ const insertEachEndpoint = async (endpoints) => {
 
 const getAllData = async (collection, endpoint) => {
     response = await getData(endpoint)
-   
     await insertData(collection, response.data.results)
     
     if(response.data.next !== null){
@@ -41,7 +40,6 @@ const getAllData = async (collection, endpoint) => {
 
 const insertData  = async (collection, documents) => {
     try {
-        // console.log(documents.length)
         const starwars = database.collection(collection);
         await starwars.insertMany(documents)
     } catch (error) {
